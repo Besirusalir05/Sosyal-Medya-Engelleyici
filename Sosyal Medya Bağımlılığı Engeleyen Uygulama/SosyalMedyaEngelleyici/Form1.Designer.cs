5206bba72d9cbca3cf7cc8a96b4da98ade38ca88
Besirusalir05 <usalirbesir816@gmail.com>
2025-05-22 00:29:23 +0300

    Add files via upload

---
+++ Sosyal Medya Bağımlılığı Engeleyen Uygulama/SosyalMedyaEngelleyici/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace SosyalMedyaEngelleyici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnİns = new System.Windows.Forms.Button();
+            this.btnYtb = new System.Windows.Forms.Button();
+            this.btnFcbk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnİns
+            // 
+            this.btnİns.BackgroundImage = global::SosyalMedyaEngelleyici.Properties.Resources.instagram;
+            this.btnİns.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnİns.Location = new System.Drawing.Point(607, 208);
+            this.btnİns.Name = "btnİns";
+            this.btnİns.Size = new System.Drawing.Size(204, 83);
+            this.btnİns.TabIndex = 2;
+            this.btnİns.UseVisualStyleBackColor = true;
+            this.btnİns.Click += new System.EventHandler(this.btnİns_Click);
+            // 
+            // btnYtb
+            // 
+            this.btnYtb.BackgroundImage = global::SosyalMedyaEngelleyici.Properties.Resources.Logo_of_YouTube__2015_2017__svg;
+            this.btnYtb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnYtb.Location = new System.Drawing.Point(329, 208);
+            this.btnYtb.Name = "btnYtb";
+            this.btnYtb.Size = new System.Drawing.Size(204, 83);
+            this.btnYtb.TabIndex = 1;
+            this.btnYtb.UseVisualStyleBackColor = true;
+            this.btnYtb.Click += new System.EventHandler(this.btnYtb_Click);
+            // 
+            // btnFcbk
+            // 
+            this.btnFcbk.BackgroundImage = global::SosyalMedyaEngelleyici.Properties.Resources.Facebook_New_Logo__2015__svg;
+            this.btnFcbk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFcbk.Location = new System.Drawing.Point(42, 208);
+            this.btnFcbk.Name = "btnFcbk";
+            this.btnFcbk.Size = new System.Drawing.Size(204, 83);
+            this.btnFcbk.TabIndex = 0;
+            this.btnFcbk.UseVisualStyleBackColor = true;
+            this.btnFcbk.Click += new System.EventHandler(this.btnFcbk_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-1, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(863, 67);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yasaklı Uygulamalar";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(296, 389);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(0, 16);
+            this.labelX.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(852, 552);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnİns);
+            this.Controls.Add(this.btnYtb);
+            this.Controls.Add(this.btnFcbk);
+            this.Name = "Form1";
+            this.Text = "Sosyal Medya Engelleyici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFcbk;
+        private System.Windows.Forms.Button btnYtb;
+        private System.Windows.Forms.Button btnİns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelX;
+    }
+}
+
---
+++ Sosyal Medya Bağımlılığı Engeleyen Uygulama/SosyalMedyaEngelleyici/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace SosyalMedyaEngelleyici
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer soundPlayer;
+        private Form xForm;
+        private Timer soundTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeXForm();
+            InitializeTimers();
+            this.DoubleBuffered = true;
+        }
+
+        private void InitializeXForm()
+        {
+            xForm = new Form
+            {
+                FormBorderStyle = FormBorderStyle.None,
+                WindowState = FormWindowState.Maximized,
+                BackColor = Color.Black,
+                Opacity = 5,
+                TopMost = true,
+                ShowInTaskbar = false
+            };
+
+            xForm.Paint += (sender, e) =>
+            {
+                
+                using (Pen redPen = new Pen(Color.Red, 20))
+                {
+                    e.Graphics.DrawLine(redPen, 100, 100, xForm.Width - 100, xForm.Height - 100);
+                    e.Graphics.DrawLine(redPen, xForm.Width - 100, 100, 100, xForm.Height - 100);
+                }
+
+                
+                using (Font font = new Font("Arial", 48, FontStyle.Bold))
+                {
+                    e.Graphics.DrawString("ERİŞİM ENGELLENDİ!",
+                                        font,
+                                        Brushes.White,
+                                        new PointF(xForm.Width / 2, xForm.Height / 2),
+                                        new StringFormat { Alignment = StringAlignment.Center });
+                }
+            };
+
+            xForm.Click += (sender, e) => HideXForm();
+            xForm.FormClosing += (sender, e) => StopSound();
+        }
+
+        private void InitializeTimers()
+        {
+            
+            soundTimer = new Timer { Interval = 3000 }; 
+            soundTimer.Tick += (s, e) => PlayErrorSound();
+        }
+
+        private void btnFcbk_Click(object sender, EventArgs e)
+        {
+            BlockAccess("Facebook");
+        }
+
+        private void btnYtb_Click(object sender, EventArgs e)
+        {
+            BlockAccess("YouTube");
+        }
+
+        private void btnİns_Click(object sender, EventArgs e)
+        {
+            BlockAccess("Instagram");
+        }
+
+        private void BlockAccess(string siteName)
+        {
+            
+            ShowXForm();
+
+            
+            PlayErrorSound();
+            soundTimer.Start();
+
+            
+            MessageBox.Show($"{siteName} erişimi tamamen engellendi!", "Erişim Engeli", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void PlayErrorSound()
+        {
+            try
+            {
+                if (soundPlayer == null)
+                {
+                    soundPlayer = new SoundPlayer("error.wav");
+                    soundPlayer.PlayLooping(); 
+                }
+                else
+                {
+                    soundPlayer.PlayLooping();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ses çalınamadı: " + ex.Message);
+            }
+        }
+
+        private void StopSound()
+        {
+            soundPlayer?.Stop();
+            soundTimer.Stop();
+        }
+
+        private void ShowXForm()
+        {
+            xForm.Show();
+        }
+
+        private void HideXForm()
+        {
+            xForm.Hide();
+            StopSound();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopSound();
+            soundPlayer?.Dispose();
+            xForm?.Dispose();
+            soundTimer?.Dispose();
+        }
+    }
+}
